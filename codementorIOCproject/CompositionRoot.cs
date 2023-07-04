4a047fa149628d6124d04b03c2d0d5862b200172
guangqi23 <56778377+guangqi23@users.noreply.github.com>
2023-07-04 11:43:57 +0800

    codementor project

---
+++ codementorIOCproject/CompositionRoot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Castle.Windsor;
+using Castle.MicroKernel.Registration;
+
+namespace codementorIOCproject
+{
+	public class CompositionRoot : ICompositionRoot
+	{
+		// Composition Root contain other application logic
+		public CompositionRoot()
+		{
+			var container = new WindsorContainer();
+
+			//Register Windsor container with interface and mapped it to it implementing CompositionRoot type
+			container.Register(Component.For<ICompositionRoot>().ImplementedBy<CompositionRoot>());
+
+			//Initialising Windsor Container to give an insance of ICompositionRoot
+			var root = container.Resolve<ICompositionRoot>();
+
+			Console.ReadLine();
+
+		}
+	}
+}
+
